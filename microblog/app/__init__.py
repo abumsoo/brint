931dfa4afb2bf100e3f3acdf5a15a27790076a5b
bumshakabum <bumcrystlbum@gmail.com>
2017-06-26 15:28:03 -0600

    basic db. Just before OAuth

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager, UserMixin
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+lm = LoginManager(app)
 
-from app import views
+from app import views, models
