d363a341af76cf2be49ebaf81b06ab02eaec0abb
Bum Kim <bumcrystlbum@gmail.com>
2017-07-27 17:47:27 -0600

    daily budget script takes into account income and expenses

--- daily_budget.rb
+++ daily_budget.rb
@@ -1,21 +1,27 @@
-#!/usr/local/bin/ruby24
-
-allowance_month = 1000.00
+allowance = 1000.00 / 30
 wage = 12.00
 hours_fortnight = 20
 tax_rate = 0.11
 save_rate = 0.15
-rent = 745.00
-internet = 38.00
-energy = 30.00
-groceries = 50.00
-phone = 30.00
 
+expenses = {
+  "rent" => 745.00,
+  "internet" => 38.00,
+  "energy" => 30.00,
+  "groceries" => 200.00,
+  "phone" => 30.00,
+}
+
+expenses.each do |key, value|
+  expenses[key] = value / 30
+end
 
 revenue = wage * hours_fortnight
 tax = revenue * tax_rate
 saving = (revenue - tax) * save_rate
 fortnight_income = revenue - tax - saving
 daily_income = fortnight_income / 14
+
+puts allowance + daily_income - expenses.values.sum
 
 puts daily_income.round(3)
